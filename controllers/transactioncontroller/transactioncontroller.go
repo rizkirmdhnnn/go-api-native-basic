86d95164d5de145468949c77d9fbd59847ca9c30
rizkirmdhnnn <achmadrizkiramadhan0101@gmail.com>
2024-01-20 12:50:41 +0700

    fix : remove spamming log

--- controllers/transactioncontroller/transactioncontroller.go
+++ controllers/transactioncontroller/transactioncontroller.go
@@ -48,7 +48,7 @@ func Create(w http.ResponseWriter, r *http.Request) {
 
 	if err := config.DB.
 		Where("member_id = ? AND book_id = ? AND return_date = ?", transaction.MemberID, transaction.BookID, "").
-		First(&transaction); err != nil {
+		Find(&transaction).Error; err != nil {
 		helper.Response(w, 409, "The member still borrowed the book and has not returned it!", nil)
 		return
 	}
@@ -121,7 +121,7 @@ func Update(w http.ResponseWriter, r *http.Request) {
 
 	if err := config.DB.
 		Where("member_id = ? AND book_id = ? AND return_date != ?", transaction.MemberID, transaction.BookID, "").
-		First(&transaction); err != nil {
+		Find(&transaction).Error; err != nil {
 		helper.Response(w, 409, "Book has been returned!", nil)
 		return
 	}
